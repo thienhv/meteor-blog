e376273d1dc9f54900f643375f26f284eb18f8b7
thienhv <thien.hoviet@gmail.com>
2015-12-21 13:05:59 +0700

    add some helpers

---
+++ client/templates/home.js
@@ -0,0 +1,35 @@
+Template.home.created = function(){
+  console.log('Created  the home  template');
+};
+
+Template.home.rendered  = function(){
+  console.log('Rendered the home  template');
+};
+
+Template.home.destroyed = function(){
+  console.log('Destroyed  the home  template');
+};
+
+Template.home.helpers({
+  exampleHelper:  function(){
+    return  'This text  came  from  a helper  with  some <strong>HTML</strong>.';
+  }
+});
+
+Template.home.helpers({
+  exampleHelper:  function(){
+    return new Spacebars.SafeString('This text came from a helper with some <strong>HTML</strong>.');
+  }
+});
+
+Template.home.helpers({
+  //  other helpers...
+  dataContextHelper:  function(){
+    return  {
+      someText: 'This text  was set using a helper  of  the parent  template.',
+      someNested: {
+          text: 'That comes from  "someNested.text"'
+      }
+    };
+  }
+});
